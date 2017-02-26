b7070b18bddbd223cb48fda072435a0c8e30ed8f
cambalamas <cambalamas@gmail.com>
2017-02-26 03:42:12 +0100

    :D

--- Packages/Default/duplicate_line.py
+++ Packages/Default/duplicate_line.py
@@ -3,11 +3,11 @@
 import sublime, sublime_plugin
 
 class DuplicateLineCommand(sublime_plugin.TextCommand):
-    def run(self, edit, force_lines=False):
-        for region in self.view.sel():
-            if force_lines or region.empty():
-                line = self.view.line(region)
-                line_contents = self.view.substr(line) + '\n'
-                self.view.insert(edit, line.begin(), line_contents)
-            else:
-                self.view.insert(edit, region.begin(), self.view.substr(region))
+	def run(self, edit, force_lines=False):
+		for region in self.view.sel():
+			if force_lines or region.empty():
+				line = self.view.line(region)
+				line_contents = self.view.substr(line) + '\n'
+				self.view.insert(edit, line.begin(), line_contents)
+			else:
+				self.view.insert(edit, region.begin(), self.view.substr(region))
